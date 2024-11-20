e002ba929154c807c7e22207d7f389bc184cdddc
sueszli <jabaryyahya@gmail.com>
2024-11-20 06:23:04 +0100

    up

--- src/0_vanilla/main.py
+++ src/0_vanilla/main.py
@@ -1,35 +1,18 @@
-def timeit(func) -> callable:
-    import functools
-    import time
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print(f"{func.__name__} executed in {end - start:.5f}s")
-        return result
-
-    return wrapper
-
-
 def sha1(msg):
     if isinstance(msg, str):
         msg = msg.encode()
     assert isinstance(msg, bytes)
 
-    lrot = lambda value, n: ((value << n) & mask) | (value >> (width - n))
-    bytes_to_word = lambda b: (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3]
-
-    h = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
-    width = 32
-    mask = 0xFFFFFFFF
     ml = len(msg) * 8
-
     msg += b"\x80"
     msg += b"\x00" * (-(len(msg) + 8) % 64)
     msg += bytes([(ml >> (56 - i * 8)) & 0xFF for i in range(8)])
 
+    width = 32
+    lrot = lambda value, n: ((value << n) & 0xFFFFFFFF) | (value >> (width - n))
+    bytes_to_word = lambda b: (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3]
+
+    h = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
     for chunk in [msg[i : i + 64] for i in range(0, len(msg), 64)]:
         w = [bytes_to_word(chunk[i : i + 4]) for i in range(0, 64, 4)]
 
@@ -46,17 +29,14 @@ def sha1(msg):
                 f, k = (b & c) | (d & (b | c)), 0x8F1BBCDC
             else:
                 f, k = b ^ c ^ d, 0xCA62C1D6
-            temp = (lrot(a, 5) + f + e + k + w[i]) & mask
-            e, d, c, b, a = d, c, lrot(b, 30), a, temp
-
-        c_hash = [a, b, c, d, e]
-        h = [((v + n) & mask) for v, n in zip(h, c_hash)]
+            tmp = (lrot(a, 5) + f + e + k + w[i]) & 0xFFFFFFFF
+            e, d, c, b, a = d, c, lrot(b, 30), a, tmp
+        h = [((v + n) & 0xFFFFFFFF) for v, n in zip(h, [a, b, c, d, e])]
 
     return b"".join([v.to_bytes(4, "big") for v in h])
 
 
-@timeit
-def crack_hash(target_hash, max_length=8):
+def search_collision(target_hash, max_length=8):
     from string import ascii_letters, digits
 
     def product(*args, repeat=1):
@@ -78,6 +58,6 @@ def crack_hash(target_hash, max_length=8):
 
 
 target = sha1("ab".encode()).hex()
-result = crack_hash(target)
+result = search_collision(target)
 print(f"Hash: {target}")
 print(f"Cracked password: {result}")
--- src/0_vanilla/sha1.py
+++ src/0_vanilla/sha1.py
@@ -12,21 +12,19 @@ def sha1(msg):
         msg = msg.encode()
     assert isinstance(msg, bytes)
 
-
-    h = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
-    width = 32
-    mask = 0xFFFFFFFF
     ml = len(msg) * 8
-
     msg += b"\x80"
     msg += b"\x00" * (-(len(msg) + 8) % 64)
     msg += bytes([(ml >> (56 - i * 8)) & 0xFF for i in range(8)])
 
-    lrot = lambda value, n: ((value << n) & mask) | (value >> (width - n))
+    width = 32
+    lrot = lambda value, n: ((value << n) & 0xFFFFFFFF) | (value >> (width - n))
     bytes_to_word = lambda b: (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3]
 
+    h = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0]
     for chunk in [msg[i : i + 64] for i in range(0, len(msg), 64)]:
         w = [bytes_to_word(chunk[i : i + 4]) for i in range(0, 64, 4)]
+
         for i in range(16, 80):
             w.append(lrot(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1))
 
@@ -40,9 +38,9 @@ def sha1(msg):
                 f, k = (b & c) | (d & (b | c)), 0x8F1BBCDC
             else:
                 f, k = b ^ c ^ d, 0xCA62C1D6
-            tmp = (lrot(a, 5) + f + e + k + w[i]) & mask
+            tmp = (lrot(a, 5) + f + e + k + w[i]) & 0xFFFFFFFF
             e, d, c, b, a = d, c, lrot(b, 30), a, tmp
-        h = [((v + n) & mask) for v, n in zip(h, [a, b, c, d, e])]
+        h = [((v + n) & 0xFFFFFFFF) for v, n in zip(h, [a, b, c, d, e])]
 
     return b"".join([v.to_bytes(4, "big") for v in h])
 
